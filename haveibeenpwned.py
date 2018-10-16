1057c011ab943cfde965d91a5aefe72cb0ff96e9
Jeff Barron <infamousjeff@gmail.com>
2018-10-16 09:05:47 -0400

    Add files via upload
    Fix missing quotes and added python 3.0 compatibility

--- haveibeenpwned.py
+++ haveibeenpwned.py
@@ -12,10 +12,8 @@ import time
 import argparse
 
 parser = argparse.ArgumentParser(description="Verify if email address has been pwned")
-parser.add_argument("-a", dest="address",
-                  help="Single email address to be checked")
-parser.add_argument("-f", dest="filename",
-                  help="File to be checked with one email addresses per line")
+parser.add_argument("-a", dest="address", help="Single email address to be checked")
+parser.add_argument("-f", dest="filename",help="File to be checked with one email addresses per line")
 args = parser.parse_args()
 
 rate = 1.3                            # 1.3 seconds is a safe value that in most cases does not trigger rate limiting
@@ -34,7 +32,7 @@ ENDC = '\033[0m'
 
 address = str(args.address)
 filename = str(args.filename)
-lstEmail = ["info@example.com","example@example.com]
+lstEmail = ["info@example.com","example@example.com"]
 
 def main():
     if address != "None":
@@ -53,21 +51,21 @@ def checkAddress(email):
                  proxies = proxies,
                  verify = sslVerify)
     if str(check.status_code) == "404": # The address has not been breached.
-        print OKGREEN + "[i] " + email + " has not been breached." + ENDC
+        print(OKGREEN + "[i] " + email + " has not been breached." + ENDC)
         time.sleep(sleep) # sleep so that we don't trigger the rate limit
         return False
     elif str(check.status_code) == "200": # The address has been breached!
-        print FAILRED + "[!] " + email + " has been breached!" + ENDC
+        print (FAILRED + "[!] " + email + " has been breached!" + ENDC)
         time.sleep(sleep) # sleep so that we don't trigger the rate limit
         return True
     elif str(check.status_code) == "429": # Rate limit triggered
-        print WARNING + "[!] Rate limit exceeded, server instructed us to retry after " + check.headers['Retry-After'] + " seconds" + ENDC
-        print WARNING + "    Refer to acceptable use of API: https://haveibeenpwned.com/API/v2#AcceptableUse" + ENDC
+        print (WARNING + "[!] Rate limit exceeded, server instructed us to retry after " + check.headers['Retry-After'] + " seconds" + ENDC)
+        print (WARNING + "    Refer to acceptable use of API: https://haveibeenpwned.com/API/v2#AcceptableUse" + ENDC)
         sleep = float(check.headers['Retry-After']) # Read rate limit from HTTP response headers and set local sleep rate
         time.sleep(sleep) # sleeping a little longer as the server instructed us to do
         checkAddress(email) # try again
     else:
-        print WARNING + "[!] Something went wrong while checking " + email + ENDC
+        print (WARNING + "[!] Something went wrong while checking " + email + ENDC)
         time.sleep(sleep) # sleep so that we don't trigger the rate limit
         return True
 
